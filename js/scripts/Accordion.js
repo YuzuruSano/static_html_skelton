e71df5b17a4a6d20b4f5a7d34c53c8d51fe77ee7
Yuzuru Sano <sano@suitosha.co,jp>
2017-02-09 01:19:23 +0900

    アコーディオンにデフォルトイベントの無効化を追加

--- js/scripts/Accordion.js
+++ js/scripts/Accordion.js
@@ -4,12 +4,15 @@ class Accordion{
 	}
 
 	exec(){
-		$(this.target).on('click',function(){
+		$(this.target).on('click',function(e){
+			e.preventDefault();
 			let next = $(this).next();
 			let state = next.css('display');
 			if(state == 'block'){
+				$(this).removeClass('active');
 				next.slideUp(200);
 			}else{
+				$(this).addClass('active');
 				next.slideDown(200);
 			}
 		});
