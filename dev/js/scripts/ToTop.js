a5414e3eb7400d03d8507adb746e818d5e9429f5
Yuzuru Sano(mac book) <sano@suitosha.co.jp>
2018-02-16 12:13:25 +0900

    トップへ移動するボタンにフッター手前での停止機能追加

--- dev/js/scripts/ToTop.js
+++ dev/js/scripts/ToTop.js
@@ -1,18 +1,46 @@
 class ToTop{
-	constructor(target,fixer,speed) {
+	constructor(target,fixer,speed,stop = true) {
 		this.target = target;
 		this.fixer = fixer;
 		this.speed = speed;
+		this.stop = stop;
+	}
+	/**
+	 * フッター手前でボタンを止める
+	 * position fixed or relativeを切り替えるだけなのでhtml構造による位置調整や左右位置などはDOM/CSSで別途調整必要
+	 * @return none
+	 */
+	clear_fixed_position(){
+		let scrollHeight = $(document).height();
+		let scrollPosition = $(window).height() + $(window).scrollTop();
+		let footHeight = $("#footer").innerHeight();
+
+		if ( scrollHeight - scrollPosition  <= footHeight ) {
+			$(this.target).css({
+				"position":"absolute",
+				"bottom": footHeight + 30
+			});
+		} else { //それ以外の場合は
+			$(this.target).css({
+				"position":"fixed",
+				"bottom": 30
+			});
+		}
 	}
 
 	exec(){
 		const [target,fixer,speed] = [this.target,this.fixer,this.speed];
+		const _this = this;
 
 		$(window).on('load scroll',function(){
 			if($(this).scrollTop() > fixer){
 				$(target).fadeIn(speed);
 			}else{
 				$(target).fadeOut(speed);
+			}
+
+			if(this.stop){
+				_this.clear_fixed_position();
 			}
 		});
 	}
