df096c3a3ec805b69c8acf84db6d45d448914571
YuzuruSano <ysano@oikaze.jp>
2020-12-26 09:29:50 +0900

    Code formatting

--- webpack.common.js
+++ webpack.common.js
@@ -2,20 +2,21 @@ const path = require("path");
 const webpack = require("webpack");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
 
-const isProduction = process.argv[process.argv.indexOf('--mode') + 1] === 'production';
+const isProduction =
+  process.argv[process.argv.indexOf("--mode") + 1] === "production";
 
 module.exports = {
   entry: {
     bundle: [path.resolve(__dirname, "./dev/js/scripts/main.js")],
-    'style.css': [path.resolve(__dirname, "./dev/sass/style.scss")],
-    'shorthands.css': [path.resolve(__dirname, "./dev/sass/shorthands.scss")]
+    "style.css": [path.resolve(__dirname, "./dev/sass/style.scss")],
+    "shorthands.css": [path.resolve(__dirname, "./dev/sass/shorthands.scss")],
   },
   output: {
-    filename: 'js/[name].js',
+    filename: "js/[name].js",
     path: path.resolve(__dirname, "./build"),
-    publicPath: "http://localhost:8080/"
+    publicPath: "http://localhost:8080/",
   },
   module: {
     rules: [
@@ -26,67 +27,67 @@ module.exports = {
           {
             loader: "file-loader",
             options: {
-              name: "[name].html"
-            }
+              name: "[name].html",
+            },
           },
           "extract-loader",
           {
             loader: "html-loader",
             options: {
               attributes: true,
-              minimize:false
-            }
+              minimize: false,
+            },
           },
           {
             loader: "pug-html-loader",
             options: {
               pretty: true,
               data: {
-                "env": JSON.stringify(process.env.NODE_ENV)
-              }
-            }
-          }
-        ]
+                env: JSON.stringify(process.env.NODE_ENV),
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
               cacheDirectory: true,
-              cacheCompression: false
-            }
-          }
-        ]
+              cacheCompression: false,
+            },
+          },
+        ],
       },
-      { test: /\.html$/, exclude: /node_modules/,loader: "html-loader" }
-    ]
+      { test: /\.html$/, exclude: /node_modules/, loader: "html-loader" },
+    ],
   },
   externals: {
-    jquery: "jQuery"
+    jquery: "jQuery",
   },
   resolve: {
     modules: ["node_modules", "dev/js/scripts"],
-    extensions: [".js", ".jsx", ".css"]
+    extensions: [".js", ".jsx", ".css"],
   },
   performance: { hints: false },
   plugins: [
     new FixStyleOnlyEntriesPlugin({
-      extensions: ['scss', 'css'],
-      ignore: 'webpack-hot-middleware'
+      extensions: ["scss", "css"],
+      ignore: "webpack-hot-middleware",
     }),
     new MiniCssExtractPlugin({
-      filename: 'css/[name]'
+      filename: "css/[name]",
     }),
     new CopyWebpackPlugin({
-      patterns: [{ from: "./dev/images", to: "images/" }]
+      patterns: [{ from: "./dev/images", to: "images/" }],
     }),
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery",
-      "window.jQuery": "jquery"
-    })
-  ]
+      "window.jQuery": "jquery",
+    }),
+  ],
 };
