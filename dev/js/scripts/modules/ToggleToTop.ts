a2b3ecdee6adc166342feb9ef5e766c138f73039
YuzuruSano <ysano@oikaze.jp>
2021-07-16 14:11:37 +0900

    feat: toggle position / visiblity on scroll top btn

--- dev/js/scripts/modules/ToggleToTop.ts
+++ dev/js/scripts/modules/ToggleToTop.ts
@@ -1,4 +1,5 @@
-import { scrollTop } from "Util";
+import Util from "./Util";
+import { throttle } from 'throttle-debounce';
 
 export default class ToggleToTop {
   private target:HTMLElement | null;
@@ -6,6 +7,7 @@ export default class ToggleToTop {
   private fixer: Number;
   private speed: Number;
   private stop: Boolean;
+  private util: Util;
   
   constructor({
     target = "#totop",
@@ -19,6 +21,27 @@ export default class ToggleToTop {
     this.fixer = fixer;
     this.speed = speed;
     this.stop = stop;
+    this.util = new Util();
+
+    this.bindEvent();
+  }
+
+  bindEvent() {
+    window.addEventListener("load", () => {
+      this.toggle();
+    })
+
+    window.addEventListener("scroll",
+      throttle(200, () => {
+        this.toggle();
+      })
+    );
+
+    window.addEventListener("resize",
+      throttle(200, () => {
+        this.toggle();
+      })
+    );
   }
 
   /**
@@ -26,43 +49,33 @@ export default class ToggleToTop {
    * position fixed or relativeを切り替えるだけなのでhtml構造による位置調整や左右位置などはDOM/CSSで別途調整必要
    * @return void
    */
-  clear_fixed_position() {
+  togglePosition() {
     if (!this.target) return;
-
-    const scrollHeight: Number = document.documentElement.clientHeight,
-          scrollPosition: Number = window.innerHeight + scrollTop(),
+    
+    const scrollHeight: Number = document.body.clientHeight,
+          scrollPosition: Number = +window.innerHeight + +this.util.scrollTop(),
           footHeight : Number = this.stop_at ? this.stop_at.clientHeight : 0,
-          trigger: Number = Number(scrollHeight) - Number(scrollPosition);
+          trigger: Number = +scrollHeight - +scrollPosition;
     
     if (trigger <= footHeight) {
+      const bottomPos = +footHeight + 30;
       this.target.style.position = "absolute";
-      this.target.style.bottom = `${Number(footHeight) + 30}px`;
+      this.target.style.bottom = `${bottomPos}px`;
     } else {
       this.target.style.position = "fixed";
       this.target.style.bottom = "30px";
     }
   }
 
-  bindEvent(){
-    document.addEventListener("load", () => {
-      this.toggle();
-    })
-
-    document.addEventListener("resize", () => {
-      this.toggle();
-    })
-  }
-
   toggle() {
-    const [target, fixer, speed] = [this.target, this.fixer, this.speed];
-    if (scrollTop() > fixer) {
-      this.target?.classList.add('._triggerd');
+    if (this.util.scrollTop() > this.fixer) {
+      this.target?.classList.add('_triggerd');
     } else {
-      this.target?.classList.remove('._triggerd');
+      this.target?.classList.remove('_triggerd');
     }
 
     if (this.stop) {
-      this.clear_fixed_position();
+      this.togglePosition();
     }
   }
 }
